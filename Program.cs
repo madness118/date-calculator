785dec34b156977ead1628d4a0c7915f0afca230
maddie <madelynchinglee@gmail.com>
2023-02-04 03:00:31 +0800

    finished GetNewDate, deleted CalculateTmeToAdd, finished project in general

--- Program.cs
+++ Program.cs
@@ -1,53 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Transactions;
 
 namespace dateCalculator
 {
     class Program
     {
+        public static string[] keywords = { "day", "week", "month", "year" };
+
+
         public static DateTime GetCurrentDate()
         {
-            DateTime now = DateTime.Now;
-
-            return now;
+            return DateTime.Now;
         }
 
-
-        public static Dictionary<int, string> CalculateTimeToAdd(string time) //calculate amount of time to add as a dictionary, separate two values in a different function
+        /*
+        public static Dictionary<int, string> CalculateTimeToAdd(string input) //calculate amount of time to add as a dictionary, separate two values in a different function
         {
-            string[] keywords = { "day", "week", "month", "year" };
-            IDictionary<int, string> timeToAdd = new Dictionary<int, string>();
-            DateTime newTime = new DateTime();
+            IDictionary<int, string> timeToAdd_dict = new Dictionary<int, string>();
 
-            while (true)
-            {
-                try
+            int n;
+
+            for (int i = 0; i < keywords.Length; i++)
+                if (input.Contains(keywords[i]) == true)
                 {
-                    Console.WriteLine("\n\nEnter a time of any length in the format <measure of time>, <quantity> :  ");
-                    time = Console.ReadLine();  
-                }
-                catch
-                {
-                    Console.WriteLine("Cannot accept. Please try again.");
-                }
-                for (int i = 0; i < keywords.Length; i++)
-                    if (time.Contains(keywords[i]) == true)
+                    string[] strings = input.Split(' ');
+                    for (int j = 0; j < strings.Length; j+=2)
                     {
-                        //keywordsUsed.Add(); add quantity, then substring containing keyword into dictionary
+                        n = int.Parse(strings[j]);
+                        timeToAdd_dict.Add(n, strings[j+=1]);
                     }
+                }
+
+            return (Dictionary<int, string>)timeToAdd_dict;
+        }
+        */
+
+
+        public static DateTime GetNewDate(string timeS, int timeI)
+        {
+            DateTime newDate = new DateTime();
+            DateTime now = GetCurrentDate();
+
+            switch (timeS)
+            {
+                case "day":
+                    newDate = now.AddDays(timeI);
+                    break;
+                case "week":
+                    newDate = now.AddDays(timeI*7);
+                    break;
+                case "month":
+                    newDate = now.AddMonths(timeI);
+                    break;
+                case "year":
+                    newDate = now.AddYears(timeI);
+                    break;
             }
 
-            return (Dictionary<int, string>)timeToAdd;
+            return newDate;
         }
-        
+
 
         static void Main(string[] args)
         {
             Console.WriteLine($"It is currently {GetCurrentDate():F}");
+            string timeType;
+            int quantity;
+
+            Console.WriteLine("Enter a singular measurement of time (e.g week not weeks): ");
+            timeType = Console.ReadLine();
+            Console.WriteLine($"Enter the quantity of {timeType}s: ");
+            quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n\nIn {quantity} {timeType}s, it will be {GetNewDate(timeType, quantity)}");
         }
     }
 }
