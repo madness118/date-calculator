165f81fcbef6dd12a79710daa95a5cabb6538fd1
madness118 <78645625+madness118@users.noreply.github.com>
2023-02-07 22:00:34 +0800

    Added classes, added IReaderWriter interface
    1) DateUtility class contains date methods
    2) IReaderWriter so that the program can be used without a console

---
+++ ReaderWriterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dateCalculator
+{
+    public enum ReaderWriterType
+    {
+        Console,
+        Html,
+        WinForm
+    }
+
+    class ReaderWriterFactory
+    {
+        public IReaderWriter Create(ReaderWriterType readerWriterType)
+        {
+            switch (readerWriterType)
+            {
+                case ReaderWriterType.Console:
+                    return new ConsoleReaderWriter();
+
+                case ReaderWriterType.Html:
+                    return null;
+
+                case ReaderWriterType.WinForm:
+                    return null;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
